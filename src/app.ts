fe65a45ebd8aa08d6db61817876c928d8df8167e
athulpm <97athulpm@gmail.com>
2023-06-14 10:26:22 +0530

    Added express route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import * as Tesseract from "tesseract.js";
 
+import * as express from 'express'
+
+const app = express();
+
+app.use('/', (req, res) => {
+  
+})
+
 Tesseract.recognize(
   "https://imgv3.fotor.com/images/blog-cover-image/How-to-Make-Text-Stand-Out-And-More-Readable.jpg",
   "eng"
@@ -7,3 +15,5 @@ Tesseract.recognize(
 ).then(({ data: { text } }) => {
   console.log(text);
 });
+
+app.listen(3000);
