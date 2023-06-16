773d4f9c876e8624f1a0b3274095bc6e064cc072
athulpm <97athulpm@gmail.com>
2023-06-16 17:13:06 +0530

    changed file structure

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import * as Tesseract from "tesseract.js";
 
 import * as express from 'express'
 
@@ -7,13 +6,5 @@ const app = express();
 app.use('/', (req, res) => {
   
 })
-
-Tesseract.recognize(
-  "https://imgv3.fotor.com/images/blog-cover-image/How-to-Make-Text-Stand-Out-And-More-Readable.jpg",
-  "eng"
-  // { logger: m => console.log(m) }
-).then(({ data: { text } }) => {
-  console.log(text);
-});
 
 app.listen(3000);
