fc3bd1edf46d04b26b5796e883bc515a1520bceb
athulpm <97athulpm@gmail.com>
2023-06-19 10:06:50 +0530

    improved route

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import * as express from 'express'
 const app = express();
 
 app.use('/', (req, res) => {
-  
+  const text = req.body.text;
 })
 
 app.listen(3000);
--- src/textProcessor/textProcessor.ts
+++ src/textProcessor/textProcessor.ts
@@ -1,9 +1,9 @@
 import * as Tesseract from "tesseract.js";
 
-Tesseract.recognize(
+export const processedText = Tesseract.recognize(
   "https://imgv3.fotor.com/images/blog-cover-image/How-to-Make-Text-Stand-Out-And-More-Readable.jpg",
   "eng"
   // { logger: m => console.log(m) }
 ).then(({ data: { text } }) => {
-  console.log(text);
+  return text;
 });
